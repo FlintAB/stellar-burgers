e62c154b08eaf6ca91a5acacf153bdf600a778bc
Aleksey Baranovskiy <lari.bro@yandex.ru>
2025-07-12 12:53:27 +0300

    fix/components & store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,15 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import ingredientsReducer from './slices/ingredients';
+import builderReducer from './slices/builder';
+import ordersReducer from './slices/orders';
+import feedsReducer from './slices/feeds';
+import userReducer from './slices/user';
+import ordersMiddleware from './middlewares';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = combineReducers({
+  ingredients: ingredientsReducer,
+  builder: builderReducer,
+  orders: ordersReducer,
+  feeds: feedsReducer,
+  user: userReducer
+});
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(ordersMiddleware),
   devTools: process.env.NODE_ENV !== 'production'
 });
 
